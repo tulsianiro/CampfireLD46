cd019d7627016275674ac8f780739c3a3e4bf1db
Rohan Tulsiani <rohantulsiani@gmail.com>
2020-04-18 03:13:21 +0800

    cleanup

--- code/game.cpp
+++ code/game.cpp
@@ -1,4 +1,4 @@
 void simulate_frame(Input *input, float dt)
 {
-    draw_quad({-220.0f, -80.0f, 0.0f}, {97.0f, 97.0f}, {1.0f, 1.0f, 1.0f});
+    draw_quad({0.0f, 0.0f, 0.0f}, {320.0f, 180.0f}, {1.0f, 1.0f, 1.0f});
 }
