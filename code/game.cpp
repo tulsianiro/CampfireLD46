28bf1c4bc7026c827a0e125d6aaf23bf4d79c6cd
Jun <junyenle@usc.edu>
2020-04-20 01:07:32 +0100

    follow cam fixed

--- code/game.cpp
+++ code/game.cpp
@@ -13,7 +13,7 @@ void simulate_frame(Input *input)
     level_update_and_render();
     player_update_and_render();
 
-    // follow follow test
+    // follow player text test
     local_persist char buf[128];
     local_persist u32 accum = 0;
     if(!(accum++ % 15))
@@ -21,7 +21,7 @@ void simulate_frame(Input *input)
         sprintf_s(buf, 128, "FPS:%d", (int)(1/dt));
     }
     hmm_v2 corrected_pos = player.pos;
-    corrected_pos -= camera.player_offset_y;
+    corrected_pos.Y -= camera.player_offset_y;
     hmm_v2 text_render_pos = ortho_coordinates_to_window(corrected_pos);
     RenderText(&font_cache[DEFAULT_FONT], {1, 0, 0, 1}, text_render_pos, buf);
 }
